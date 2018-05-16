464620681af68a21b7f10f803429c2dedee07d91
Eli <evosniak@technifabinc.com>
2018-05-16 14:15:05 -0400

    Create PHP file for receiving data from AJAX post

---
+++ safetyForm.php
@@ -0,0 +1,6 @@
+<?php 
+    set_time_limit( 0 ); //Phenomical Cosmic Powahhh! Eeny bitty living space.
+    header("Content-Type: application/json; charset=UTF-8");
+    $obj = json_decode($_POST["form"], false);
+    
+?>
